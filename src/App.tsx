da2a162c2e86575e057436c9da8d92ceff1f96e2
Ben Foxall <benfoxall@gmail.com>
2021-01-09 17:24:46 +0000

    Base functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
+
+const points = "0 1 2 3 5 8 13 21 34 55 89 144 ∞ ? ☕️".split(" ");
+
 export const App = () => {
-  const [get, set] = useState(0);
+  const [selected, setSelected] = useState<string>(null);
 
   return (
     <>
-      <h1>Hello, React ({get})</h1>
-      <button onClick={() => set((i) => i + 1)}>+</button>
+      <nav>
+        {points.map((n) => (
+          <button key={n} onClick={() => setSelected(n)}>
+            {n}
+          </button>
+        ))}
+      </nav>
+      {selected && (
+        <div className="overlay">
+          <button onClick={() => setSelected(null)}>{selected}</button>
+        </div>
+      )}
     </>
   );
 };
