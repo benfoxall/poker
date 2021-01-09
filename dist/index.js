bfc27813647068be3d02f2382606f4c3c4d3394b
benfoxall <benfoxall@gmail.com>
2021-01-09 19:05:24 +0000

    Deploying to gh-pages from  @ 50785290a9962406bb2e5ce6ea30e511e0e6cea6 🚀

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,10 @@
+import __SNOWPACK_ENV__ from '../__snowpack__/env.js';
+import.meta.env = __SNOWPACK_ENV__;
+
 import React from "../web_modules/react.js";
 import {render} from "../web_modules/react-dom.js";
 import {App} from "./App.js";
 render(/* @__PURE__ */ React.createElement(App, null), document.getElementById("root"));
+if (import.meta.env.MODE === "production" && "serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js");
+}
