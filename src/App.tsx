2a6f913dfc300a3cd61019e8da4344fea413ade6
Ben Foxall <benfoxall@gmail.com>
2021-01-09 17:50:37 +0000

    Styling and animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
 
 const points = "0 1 2 3 5 8 13 21 34 55 89 144 ∞ ? ☕️".split(" ");
 
@@ -6,19 +7,31 @@ export const App = () => {
   const [selected, setSelected] = useState<string>(null);
 
   return (
-    <>
+    <AnimateSharedLayout type="crossfade">
       <nav>
         {points.map((n) => (
-          <button key={n} onClick={() => setSelected(n)}>
+          <motion.button
+            key={n}
+            layoutId={n}
+            onClick={() => setSelected(n)}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 1 }}
+          >
             {n}
-          </button>
+          </motion.button>
         ))}
       </nav>
-      {selected && (
-        <div className="overlay">
-          <button onClick={() => setSelected(null)}>{selected}</button>
-        </div>
-      )}
-    </>
+      <AnimatePresence>
+        {selected && (
+          <motion.button
+            onClick={() => setSelected(null)}
+            layoutId={selected}
+            className="overlay"
+          >
+            {selected}
+          </motion.button>
+        )}
+      </AnimatePresence>
+    </AnimateSharedLayout>
   );
 };
