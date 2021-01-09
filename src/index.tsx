50785290a9962406bb2e5ce6ea30e511e0e6cea6
Ben Foxall <benfoxall@gmail.com>
2021-01-09 19:04:20 +0000

    SW and css tweaks

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,8 @@ import { render } from "react-dom";
 import { App } from "./App";
 
 render(<App />, document.getElementById("root"));
+
+// @ts-ignore
+if (import.meta.env.MODE === "production" && "serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js");
+}
